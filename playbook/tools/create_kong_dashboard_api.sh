9231f399004346906dde8b43a564f2d35eeba497
SURFACE-SONNY\klzsy <klzsysy@gmail.com>
2018-04-24 03:33:10 +0800

    优化代码

--- playbook/tools/create_kong_dashboard_api.sh
+++ playbook/tools/create_kong_dashboard_api.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-curl -X POST -H "Content-Type: application/json" -d '{"name":"kong-dashborad", "uris":"/kong-dashborad/", "upstream_url":"http://kong-dashboard.default:8080"}' $1:8001/apis/
+curl -X POST -H "Content-Type: application/json" -d '{"name":"kong-dashboard", "uris":"/kong-dashboard/", "upstream_url":"http://kong-dashboard.default:8080"}' $1:8001/apis/
